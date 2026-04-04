b9592a0daa5295b1b273d769b1d1b79688057814
Ujjawal saini <ujjawalsaini1@gmail.com>
2026-04-04 19:44:51 +0530

    add versel capiblity in client

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import useAuthStore from '@store/authStore';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api/v1',
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000/api/v1',
   withCredentials: true, // Important for httpOnly cookies
 });
 
